5ffe09a2d4fcba05227400de9b89c4de58c8a818
艾禄 <im.ailu@outlook.com>
2024-06-01 09:06:33 +0800

    Initial Commit

---
+++ LemonCalculator/LemonCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonCalculatorApp.swift
+//  LemonCalculator
+//
+//  Created by ailu on 2024/6/1.
+//
+
+import SwiftUI
+
+@main
+struct LemonCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
